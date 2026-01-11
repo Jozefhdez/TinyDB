d3ae7146947caf31582167724f24e2d7fd891d02
jozefhdez <jozefhdez2@gmail.com>
2026-01-11 02:02:14 +0000

    feat: add internal node insertion and update functionality with related tests

--- include/btree.h
+++ include/btree.h
@@ -70,6 +70,7 @@ uint32_t *internal_node_right_child(void *node);
 uint32_t *internal_node_cell(void *node, uint32_t cell_num);
 uint32_t *internal_node_child(void *node, uint32_t child_num);
 uint32_t *internal_node_key(void *node, uint32_t key_num);
+void update_internal_node_key(void *node, uint32_t old_key, uint32_t new_key);
 uint32_t get_node_max_key(void *node);
 void create_new_root(Table *table, uint32_t right_child_page_num);
 void leaf_node_insert(Cursor *cursor, uint32_t key, Row *value);
@@ -77,11 +78,15 @@ bool is_node_root(void *node);
 void set_node_root(void *node, bool is_root);
 void leaf_node_split_and_insert(Cursor *cursor, uint32_t key, Row *value);
 Cursor *leaf_node_find(Table *table, uint32_t page_num, uint32_t key);
+uint32_t *node_parent(void *node);
 NodeType get_node_type(void *node);
 void set_node_type(void *node, NodeType type);
 void indent(uint32_t level);
 void print_tree(Pager *pager, uint32_t page_num, uint32_t indentation_level);
+uint32_t internal_node_find_child(void *node, uint32_t key);
 Cursor *internal_node_find(Table *table, uint32_t page_num, uint32_t key);
 uint32_t *leaf_node_next_leaf(void *node);
+void internal_node_insert(Table *table, uint32_t parent_page_num,
+                          uint32_t child_page_num);
 
 #endif
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -31,12 +31,12 @@ def test_insert_and_select_row():
     assert "(1, user1, person1@example.com)" in result[1]
 
 def test_insert_fails_when_table_full():
-    # attempt one more than TABLE_MAX_ROWS to trigger the error
-    commands = [f"insert {i} user{i} person{i}@example.com" for i in range(1, 1402)]
+
+    commands = [f"insert {i} user{i} person{i}@example.com" for i in range(1, 100)]
     commands.append(".exit")
     result = run_script(commands)
 
-    assert any("Error: Table full." in line for line in result)
+    assert any("Need to implement splitting internal node" in line for line in result)
 
 def test_insert_allows_max_length_strings():
     long_username = "a" * 32
@@ -172,6 +172,43 @@ def test_allows_printing_out_structure_of_3_leaf_node_btree():
     ]
     
     assert btree_output == expected
+
+def test_allows_printing_out_the_structure_of_a_4_leaf_node():
+    script = [
+        "insert 18 user18 person18@example.com",
+        "insert 7 user7 person7@example.com",
+        "insert 10 user10 person10@example.com",
+        "insert 29 user29 person29@example.com",
+        "insert 23 user23 person23@example.com",
+        "insert 4 user4 person4@example.com",
+        "insert 14 user14 person14@example.com",
+        "insert 30 user30 person30@example.com",
+        "insert 15 user15 person15@example.com",
+        "insert 26 user26 person26@example.com",
+        "insert 22 user22 person22@example.com",
+        "insert 19 user19 person19@example.com",
+        "insert 2 user2 person2@example.com",
+        "insert 1 user1 person1@example.com",
+        "insert 21 user21 person21@example.com",
+        "insert 11 user11 person11@example.com",
+        "insert 6 user6 person6@example.com",
+        "insert 20 user20 person20@example.com",
+        "insert 5 user5 person5@example.com",
+        "insert 8 user8 person8@example.com",
+        "insert 9 user9 person9@example.com",
+        "insert 3 user3 person3@example.com",
+        "insert 12 user12 person12@example.com",
+        "insert 27 user27 person27@example.com",
+        "insert 17 user17 person17@example.com",
+        "insert 16 user16 person16@example.com",
+        "insert 13 user13 person13@example.com",
+        "insert 24 user24 person24@example.com",
+        "insert 25 user25 person25@example.com",
+        "insert 28 user28 person28@example.com",
+        ".btree",
+        ".exit",
+    ]
+    result = run_script(script)
 
 def test_prints_all_rows_in_multi_level_tree():
     script = []
