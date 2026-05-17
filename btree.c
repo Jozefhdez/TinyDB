1d3295b5c8cc20617c20397ac93b119f37ef8c5e
Jozef David Hernández Campos <jozefhdez2@gmail.com>
2026-05-17 03:55:23 +0000

    feat: update internal node max cells from hardcoded value to 510

--- btree.c
+++ btree.c
@@ -59,8 +59,7 @@ const uint32_t INTERNAL_NODE_KEY_SIZE = sizeof(uint32_t);
 const uint32_t INTERNAL_NODE_CHILD_SIZE = sizeof(uint32_t);
 const uint32_t INTERNAL_NODE_CELL_SIZE =
     INTERNAL_NODE_CHILD_SIZE + INTERNAL_NODE_KEY_SIZE;
-const uint32_t INTERNAL_NODE_MAX_CELLS =
-    3; // hardcoded just for testing purposes
+const uint32_t INTERNAL_NODE_MAX_CELLS = 510;
 
 #define INVALID_PAGE_NUM UINT32_MAX
 
